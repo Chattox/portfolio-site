be223cad823bf78d536117c251101f1094538ced
Chattox <conradlineker@gmail.com>
2020-10-02 17:57:37 +0100

    Updates

---
+++ precache-manifest.ebc0021b066bb2c8f03687765673e54a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9c60246e4a541ccc0e40cf8dcb55b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d17f8051d746189f56e0",
+    "url": "/static/css/main.3ca2197e.chunk.css"
+  },
+  {
+    "revision": "207871a4c3e2fe91fb33",
+    "url": "/static/js/2.20fea808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20fea808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17f8051d746189f56e0",
+    "url": "/static/js/main.c2100554.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "7f1afa8082f5e1a377b9e0d4eb832816",
+    "url": "/static/media/helpinghand-logo.7f1afa80.png"
+  },
+  {
+    "revision": "0b221eca8849573dfe3f2d75a6d48815",
+    "url": "/static/media/logoIcon.0b221eca.svg"
+  },
+  {
+    "revision": "ad081e647c29f905a17e1a48eb2e9447",
+    "url": "/static/media/slavbot.ad081e64.jpg"
+  },
+  {
+    "revision": "b7ac7d38f43b517daaa6fdbabca2d9c8",
+    "url": "/static/media/splash.b7ac7d38.jpg"
+  }
+]);
