be42a8fbe6f5480b55b55206d65161db9961caf8
Chattox <conradlineker@gmail.com>
2022-10-01 15:38:12 +0100

    feat(about me): update work experience with links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,18 +17,17 @@ const About = () => {
       <div>
         <h1>TL;DR</h1>
         <p>
-          I am a driven, quick learning, junior software developer currently
-          working with C#.NET as a back-end engineer. My diverse background across a wide range of problem
+          I am a driven, quick learning, junior software developer currently working with React, Javascript, and Typescript as a front end developer. My diverse background across a wide range of problem
           solving situations and long history working with computers allows me
           to approach issues in an effective and practical manner.
         </p>
         <h1>My experience:</h1>
         <ul>
-          <li><b>2021 - Present:</b> Junior Software Engineer at The Football Pools</li>
-          <li><b>2020 - 2021:</b> Apprentice Junior Developer at Tote UK</li>
+          <li><b>2021 - present:</b> Junior Front-end Software Engineer at <a href="https://amplience.com" target="_blank" rel="noopener noreferrer">Amplience</a></li>
+          <li><b>2021:</b> Junior Back-end Software Engineer at <a href="https://www.footballpools.com" target="_blank" rel="noopener noreferrer">The Football Pools</a></li>
+          <li><b>2020 - 2021:</b> Apprentice Full stack Junior Developer at <a href="https://www.uktotegroup.com/" target="_blank" rel="noopener noreferrer">UK Tote Group</a></li>
           <li>
-            <b>2020:</b> Graduate of the Software Development Bootcamp at Northcoders
-            Manchester
+            <b>2020:</b> Graduate of the Software Development Bootcamp at <a href="https://northcoders.com" target="_blank" rel="noopener noreferrer">Northcoders Manchester</a>
           </li>
           <li>
             Various solo and team projects using a variety of technologies
