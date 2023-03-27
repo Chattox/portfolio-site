f3ef4bafa2519c8e65d9d319028106b08429cb2c
Chattox <conradlineker@gmail.com>
2023-03-27 15:46:41 +0100

    chore: cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
+    <App />
   </React.StrictMode>
 );
