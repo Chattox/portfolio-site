fa281d71509b56c1c3e9ebe670968a190e333942
Chattox <conradlineker@gmail.com>
2023-03-28 22:54:48 +0100

    refactor(experience): images folder

--- src/pages/Experience/ExperienceItem/expItemData.ts
+++ src/pages/Experience/ExperienceItem/expItemData.ts
@@ -6,27 +6,27 @@ export const expItemData: Array<ExperienceItemProps> = [
     dates: ' NOV 2021 - PRESENT',
     content:
       'Front end junior JS software engineer working remotely as part of an agile team building and shipping high quality code for use by our clients. Work includes research spikes, designing and building new projects starting at the ground floor, and updating and building upon existing projects.',
-    imgSrc: require('../../../images/amplience-logo.png'),
+    imgSrc: require('./images/amplience-logo.png'),
   },
   {
     title: 'The Football Pools',
     dates: 'MAR 2021 - OCT 2021',
     content:
       'Back end C# junior software engineer working completely remotely both independently and as part of a team to plan, design, build, and ship high quality code. Work included updates and features for legacy code, creating completely new projects from the ground up, and maintaining both as part of an LTS plan.',
-    imgSrc: require('../../../images/football-pools-logo.png'),
+    imgSrc: require('./images/football-pools-logo.png'),
   },
   {
     title: 'UK Tote Group',
     dates: 'JUN 2020 - FEB 2021',
     content:
       'Full stack junior software engineer position working in a fully agile team to deliver quality front and back end code. Stacks include C#, JavaScript, React, MySQL, Go, and PHP.  On the training side, I studied towards a level 4 certificate in software design while keeping up with my team and working through sprints. This position was fully remote, so I have good experience working as part of a cohesive team while working completely from home.',
-    imgSrc: require('../../../images/tote-logo.png'),
+    imgSrc: require('./images/tote-logo.png'),
   },
   {
     title: 'Northcoders',
     dates: 'JAN 2020 - APR 2020',
     content:
       'Full stack coding bootcamp in Manchester teaching a practical, industry-led curriculum. Technologies learned include JavaScript, SQL, React, and Express, as well as professional skills such as TDD, version control (git), and pair/mob programming in an agile environment. Culminated in a 2 week full stack project completed in a team of three taken from idea to MVP using a varied tech stack on both front and back end.',
-    imgSrc: require('../../../images/northcoders-logo.png'),
+    imgSrc: require('./images/northcoders-logo.png'),
   },
 ];
