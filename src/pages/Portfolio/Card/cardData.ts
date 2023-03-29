5fc1f707289ea18e28361a42734ab82d109ec43a
Chattox <conradlineker@gmail.com>
2023-03-29 10:24:10 +0100

    refactor: portfolio cards

---
+++ src/pages/Portfolio/Card/cardData.ts
@@ -0,0 +1,60 @@
+import { PortfolioCardProps } from '.';
+
+export const cardData: Array<PortfolioCardProps> = [
+  {
+    cardImgSrc: require('./images/slavbot.jpg'),
+    tags: ['Discord', 'JS', 'Back-end'],
+    title: 'Slavbot',
+    description: 'A modular, lightweight Discord bot with an easily expandable command structure',
+    links: [
+      {
+        url: 'https://github.com/chattox/slavbot_v3',
+        icon: 'github',
+      },
+    ],
+    imageContain: false,
+  },
+  {
+    cardImgSrc: require('./images/helpinghand-logo.png'),
+    tags: ['Mobile', 'Dart', 'JS', 'Full-stack'],
+    title: 'HelpingHand',
+    description: 'Cross-platform mobile app helping the elderly and vulnerable with their shopping',
+    links: [
+      {
+        url: 'https://www.youtube.com/watch?v=0aof27FyHCc',
+        icon: 'youtube',
+      },
+    ],
+    imageContain: true,
+    bgColor: '#66BB6A',
+  },
+  {
+    cardImgSrc: require('./images/raddle.png'),
+    tags: ['Webapp', 'React', 'JS', 'Front-end'],
+    title: 'Raddle',
+    description: 'A World clone built in React, because all the cool kids are doing it these days',
+    links: [
+      {
+        url: 'https://github.com/chattox/raddle',
+        icon: 'github',
+      },
+    ],
+    imageContain: true,
+    bgColor: '#FFF',
+  },
+  {
+    cardImgSrc: require('./images/rainbow-fitbit.png'),
+    tags: ['Fitbit', 'JS', 'Front-end'],
+    title: 'Simple Rainbow',
+    description:
+      'Clean, simple, custom Fitbit clockface with time, date, battery % and real time heart rate display',
+    links: [
+      {
+        url: 'https://github.com/chattox/fitbit-custom-clock-faces/tree/main/simple-rainbow',
+        icon: 'github',
+      },
+    ],
+    imageContain: true,
+    bgColor: '#000',
+  },
+];
---
+++ src/pages/Portfolio/Card/index.tsx
@@ -0,0 +1,86 @@
+import {
+  ActionIcon,
+  BackgroundImage,
+  Badge,
+  Card,
+  Center,
+  Flex,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
+import { motion } from 'framer-motion';
+import { IconBrandGithub } from '@tabler/icons-react';
+
+import { AnimatedButtonWrapper } from '../../../components/AnimatedButtonWrapper';
+import { useStyles } from './index.styles';
+import { getColor } from '../../../utils/getColor';
+
+export interface LinkProps {
+  url: string;
+  icon: string;
+}
+
+export interface PortfolioCardProps {
+  cardImgSrc: string;
+  tags?: Array<string>;
+  title: string;
+  description: string;
+  links?: Array<LinkProps>;
+  imageContain?: boolean;
+  bgColor?: string;
+}
+
+const Icons: Record<string, JSX.Element> = {
+  github: <IconBrandGithub size={48} />,
+};
+
+export const PortfolioCard = ({
+  cardImgSrc,
+  tags,
+  title,
+  description,
+  links,
+  imageContain,
+  bgColor,
+}: PortfolioCardProps) => {
+  const { classes, cx } = useStyles();
+
+  return (
+    <motion.div whileHover={{ y: -10 }}>
+      <Card shadow="sm" padding="md" radius="md" maw={'16rem'} withBorder>
+        <Card.Section>
+          <BackgroundImage
+            src={cardImgSrc}
+            className={
+              imageContain ? cx(classes.cardImage, classes.cardImageContain) : classes.cardImage
+            }
+            component={Flex}
+            sx={{ backgroundColor: bgColor }}
+          >
+            <Stack align="flex-end" justify="flex-end" spacing={3}>
+              {tags?.map((tag) => (
+                <Badge>{tag}</Badge>
+              ))}
+            </Stack>
+          </BackgroundImage>
+        </Card.Section>
+        <Title order={2}>{title}</Title>
+        <Text size="sm" color="dimmed">
+          {description}
+        </Text>
+        <Center>
+          {links?.map((link) => (
+            <AnimatedButtonWrapper>
+              <a href={link.url} target="_blank" rel="noopener">
+                <ActionIcon size="xl" variant="subtle" color={getColor()}>
+                  {Icons[link.icon]}
+                </ActionIcon>
+              </a>
+            </AnimatedButtonWrapper>
+          ))}
+        </Center>
+      </Card>
+    </motion.div>
+  );
+};
