7541bf8a57dbe23e398552692a4f3ae8b743167f
Chattox <conradlineker@gmail.com>
2023-03-30 22:25:00 +0100

    feat(experience): scroll buttons

--- src/pages/Experience/ExperienceItem/index.styles.tsx
+++ src/pages/Experience/ExperienceItem/index.styles.tsx
@@ -2,10 +2,26 @@ import { createStyles } from '@mantine/core';
 
 interface ExperienceStyles {
   side?: 'left' | 'right';
+  firstOrLast?: 'first' | 'last';
 }
 
 export const useStyles = createStyles<string, ExperienceStyles>((theme, { side }) => ({
   expContainer: {
+    alignSelf: side === 'left' ? 'flex-start' : 'flex-end',
+    display: 'flex',
+    marginTop: 250,
+    marginBottom: 350,
+    marginLeft: 16,
+    marginRight: 16,
+    zIndex: 999,
+  },
+  expScrollButton: {
+    color:
+      theme.colorScheme === 'light'
+        ? theme.fn.rgba(theme.colors.gray[3], 0.75)
+        : theme.fn.rgba(theme.colors.gray[9], 0.5),
+  },
+  expItem: {
     width: 750,
     backgroundColor:
       theme.colorScheme === 'light'
@@ -13,12 +29,6 @@ export const useStyles = createStyles<string, ExperienceStyles>((theme, { side }
         : theme.fn.rgba(theme.colors.gray[9], 0.5),
     borderRadius: theme.radius.lg,
     display: 'flex',
-    alignSelf: side === 'left' ? 'flex-start' : 'flex-end',
-    marginTop: 250,
-    marginBottom: 350,
-    marginLeft: 16,
-    marginRight: 16,
-    zIndex: 999,
   },
   textContent: {
     margin: '16px',
