22ae5ca326f13cb1c65e0630cd2e993f053a47dd
Chattox <conradlineker@gmail.com>
2023-04-06 21:38:48 +0100

    feat(header): logo

---
+++ src/content/main/images/bindrune.tsx
@@ -0,0 +1,47 @@
+interface BindruneProps {
+  height: string;
+  width: string;
+  strokeWidth: string;
+  color?: string;
+  linecap?: 'butt' | 'round' | 'square' | undefined;
+  corners?: 'round' | 'miter' | 'bevel' | undefined;
+  className?: string;
+}
+
+export const Bindrune = ({
+  height,
+  width,
+  strokeWidth,
+  color,
+  linecap,
+  corners,
+  className,
+}: BindruneProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 1024 1024"
+      className={className}
+    >
+      <path
+        id="Bindrune Logo"
+        fill="none"
+        stroke={color}
+        stroke-width={strokeWidth}
+        strokeLinecap={linecap}
+        strokeLinejoin={corners}
+        d="M 512.00,960.00
+           C 512.00,960.00 288.00,736.00 288.00,736.00
+             288.00,736.00 512.00,512.00 512.00,512.00
+             512.00,512.00 736.00,288.00 736.00,288.00
+             736.00,288.00 512.00,64.00 512.00,64.00
+             512.00,64.00 512.00,960.00 512.00,960.00M 512.00,736.00
+           C 512.00,736.00 736.00,512.00 736.00,512.00
+             736.00,512.00 512.00,288.00 512.00,288.00M 736.00,736.00
+           C 736.00,736.00 512.00,512.00 512.00,512.00"
+      />
+    </svg>
+  );
+};
--- src/desktop/Main/index.tsx
+++ src/desktop/Main/index.tsx
@@ -1,6 +1,7 @@
-import { Container, Title, Tabs, Group } from '@mantine/core';
+import { Container, Title, Tabs, Group, useMantineTheme } from '@mantine/core';
 
 import { useStyles } from './index.styles';
+import { Bindrune } from '../../content/main/images/bindrune';
 import { ThemeSwitcher } from '../../components/ThemeSwitcher';
 import { About } from '../About';
 import { Experience } from '../Experience';
@@ -8,13 +9,25 @@ import { Portfolio } from '../Portfolio';
 
 export const Main = () => {
   const { classes } = useStyles();
+  const theme = useMantineTheme();
 
   return (
     <Container sx={{ height: '100%' }} fluid={true} className={classes.mainContainer}>
       <Group position="apart">
-        <Title order={1} size="60" inline={true} className={classes.title} data-testid="header">
-          Conrad Lineker-Jones
-        </Title>
+        <Group>
+          <Bindrune
+            height="6rem"
+            width="6rem"
+            strokeWidth="4rem"
+            color={theme.fn.primaryColor()}
+            linecap="round"
+            corners="round"
+            className={classes.logo}
+          />
+          <Title order={1} size="60" inline={true} className={classes.title} data-testid="header">
+            Conrad Lineker-Jones
+          </Title>
+        </Group>
         <ThemeSwitcher size="xl" />
       </Group>
       <Tabs defaultValue="about" keepMounted={false}>
