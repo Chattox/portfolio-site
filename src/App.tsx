327832ecd47a89c6f6e5b9d5a7eda2d9b01fe6ca
Chattox <conradlineker@gmail.com>
2023-04-08 23:03:07 +0100

    feat(theme): get initial light/dark mode from system settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import { useState } from 'react';
+import { Center, ColorScheme, ColorSchemeProvider, Loader, MantineProvider } from '@mantine/core';
+import { useColorScheme, useMediaQuery } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
 
 import { lightTheme, darkTheme } from './styles/themes';
 import { Main as DesktopMain } from './desktop/Main';
@@ -8,10 +8,26 @@ import { Main as MobileMain } from './mobile/Main';
 import { PortfolioGlobalStyles } from './styles/PortfolioGlobalStyles';
 
 export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const systemColorScheme = useColorScheme();
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   const isMobile = useMediaQuery('(max-width: 900px)');
+
+  useEffect(() => {
+    const onPageLoad = () => {
+      setIsLoaded(true);
+      setColorScheme(systemColorScheme);
+    };
+
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad, false);
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, [systemColorScheme]);
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
@@ -21,7 +37,17 @@ export default function App() {
         withNormalizeCSS
       >
         <PortfolioGlobalStyles />
-        {isMobile ? <MobileMain /> : <DesktopMain />}
+        {isLoaded ? (
+          isMobile ? (
+            <MobileMain />
+          ) : (
+            <DesktopMain />
+          )
+        ) : (
+          <Center>
+            <Loader />
+          </Center>
+        )}
       </MantineProvider>
     </ColorSchemeProvider>
   );
