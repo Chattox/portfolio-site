1764eeb7eaf3782fe78a833b5d930afce5968957
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2023-06-02 11:49:30 +0100

    fix(about): typo

--- src/content/about/aboutData.ts
+++ src/content/about/aboutData.ts
@@ -2,7 +2,7 @@ export const aboutData = {
   image: require('./images/rad-face.jpg'),
   title: "Hi, I'm Conrad",
   content: [
-    "I'm a driven, quick learning, junior software engineer currently working with React, Typescript, and Javascript as a front end developer. My diverse background across a wide range of problem solving situations and long history working with computers allows me to approach issues in an effective an practical manner.",
+    "I'm a driven, quick learning, junior software engineer currently working with React, Typescript, and Javascript as a front end developer. My diverse background across a wide range of problem solving situations and long history working with computers allows me to approach issues in an effective and practical manner.",
     "Over the years I've had the opportunity to work on a wide range of projects, from personal and hobby pieces to enterprise level software. I've learned a lot along the way about what it takes to build successful software, and I'm always eager to keep learning and growing.",
     "When I'm not coding, you can usually find me tinkering with intergrated tech (think Raspberry Pi, Arduinos, etc) or working on personal projects. I'm a big believer in the power of side projects to help you stay curious, constantly learning, and engaged with the world of technology.",
     "In my free time I enjoy painting miniatures, spending time with family and friends, and seeking out more weird niche prog bands. I'm always up for trying new restaurants or checking out a new museum exhibit.",
