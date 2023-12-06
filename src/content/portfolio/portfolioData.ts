e87c7b5da45c51f936111f932d419e1e4c54d2ef
Chattox <conradlineker@gmail.com>
2023-12-06 11:13:48 +0000

    feat(portfolio): add hypedb and picam

--- src/content/portfolio/portfolioData.ts
+++ src/content/portfolio/portfolioData.ts
@@ -17,128 +17,190 @@ export interface PortfolioProps {
 
 export const portfolioData: Array<PortfolioProps> = [
   {
-    imgSrc: { light: require('./images/slavbot.jpg'), dark: require('./images/slavbot.jpg') },
-    tags: ['Discord', 'JS', 'Back-end'],
-    title: 'Slavbot',
-    tagline: 'Modular Discord bot',
-    description: 'A modular, lightweight Discord bot with an easily expandable command structure',
+    imgSrc: {
+      light: require("./images/hypedb.png"),
+      dark: require("./images/hypedb.png"),
+    },
+    tags: ["Webapp", "Typescript", "GraphQL", "MongoDB", "Full-stack"],
+    title: "HypeDB",
+    tagline:
+      "Keeping track of the 2 billion unreleased games I'm interested in",
+    description:
+      "Webapp for keeping tabs on all the unreleased games I'm interested in without clogging up my Steam wishlist",
     links: [
       {
-        url: 'https://github.com/chattox/slavbot_v3',
-        icon: 'github',
+        url: "https://github.com/Chattox/hypedb-be-v2",
+        icon: "github",
+        tooltip: "Back-end",
+      },
+      {
+        url: "https://github.com/Chattox/hypedb-frontend",
+        icon: "github",
+        tooltip: "Front-end",
+      },
+    ],
+    imageContain: true,
+    bgColor: "#DFCCFB",
+  },
+  {
+    imgSrc: {
+      light: require("./images/picam.jpg"),
+      dark: require("./images/picam.jpg"),
+    },
+    tags: ["Webapp", "Raspi", "Typescript", "Python", "Full-stack"],
+    title: "Picam",
+    tagline: "Remotely accessible camera and environment monitoring",
+    description:
+      "A Pi-based camera and environment monitor, originally made in a few hours to help rescue an escaped house cat",
+    links: [
+      {
+        url: "https://github.com/Chattox/picam-backend",
+        icon: "github",
+        tooltip: "Back-end",
+      },
+      {
+        url: "https://github.com/Chattox/picam",
+        icon: "github",
+        tooltip: "Front-end",
+      },
+    ],
+  },
+  {
+    imgSrc: {
+      light: require("./images/slavbot.jpg"),
+      dark: require("./images/slavbot.jpg"),
+    },
+    tags: ["Discord", "JS", "Back-end"],
+    title: "Slavbot",
+    tagline: "Modular Discord bot",
+    description:
+      "A modular, lightweight Discord bot with an easily expandable command structure",
+    links: [
+      {
+        url: "https://github.com/chattox/slavbot_v3",
+        icon: "github",
       },
     ],
     imageContain: false,
   },
   {
     imgSrc: {
-      light: require('./images/helpinghand-logo.png'),
-      dark: require('./images/helpinghand-logo.png'),
+      light: require("./images/helpinghand-logo.png"),
+      dark: require("./images/helpinghand-logo.png"),
     },
-    tags: ['Mobile', 'Dart', 'JS', 'Full-stack'],
-    title: 'HelpingHand',
-    tagline: 'Mobile app helping the vulnerable with their shopping',
-    description: 'Cross-platform mobile app helping the elderly and vulnerable with their shopping',
+    tags: ["Mobile", "Dart", "JS", "Full-stack"],
+    title: "HelpingHand",
+    tagline: "Mobile app helping the vulnerable with their shopping",
+    description:
+      "Cross-platform mobile app helping the elderly and vulnerable with their shopping",
     links: [
       {
-        url: 'https://github.com/Chattox/helping-hand-backend',
-        icon: 'github',
-        tooltip: 'Back-end',
+        url: "https://github.com/Chattox/helping-hand-backend",
+        icon: "github",
+        tooltip: "Back-end",
       },
       {
-        url: 'https://github.com/Chattox/helping-hand-frontend',
-        icon: 'github',
-        tooltip: 'Front-end',
+        url: "https://github.com/Chattox/helping-hand-frontend",
+        icon: "github",
+        tooltip: "Front-end",
       },
       {
-        url: 'https://www.youtube.com/watch?v=0aof27FyHCc',
-        icon: 'youtube',
+        url: "https://www.youtube.com/watch?v=0aof27FyHCc",
+        icon: "youtube",
       },
     ],
     imageContain: true,
-    bgColor: '#66BB6A',
-  },
-  {
-    imgSrc: { light: require('./images/raddle.png'), dark: require('./images/raddle.png') },
-    tags: ['Webapp', 'React', 'JS', 'Front-end'],
-    title: 'Raddle',
-    tagline: 'React-based Wordle clone',
-    description: 'A Wordle clone built in React, because all the cool kids are doing it these days',
-    links: [
-      {
-        url: 'https://github.com/chattox/raddle',
-        icon: 'github',
-      },
-      {
-        url: 'https://raddle.netlify.app',
-        icon: 'play',
-      },
-    ],
-    imageContain: true,
-    bgColor: '#FFF',
+    bgColor: "#66BB6A",
   },
   {
     imgSrc: {
-      light: require('./images/rainbow-fitbit.png'),
-      dark: require('./images/rainbow-fitbit.png'),
+      light: require("./images/raddle.png"),
+      dark: require("./images/raddle.png"),
     },
-    tags: ['Fitbit', 'JS', 'Front-end'],
-    title: 'Simple Rainbow',
-    tagline: 'Custom Fitbit clockface',
+    tags: ["Webapp", "React", "JS", "Front-end"],
+    title: "Raddle",
+    tagline: "React-based Wordle clone",
     description:
-      'Clean, simple, custom Fitbit clockface with time, date, battery % and real time heart rate display',
+      "A Wordle clone built in React, because all the cool kids are doing it these days",
     links: [
       {
-        url: 'https://github.com/chattox/fitbit-custom-clock-faces/tree/main/simple-rainbow',
-        icon: 'github',
+        url: "https://github.com/chattox/raddle",
+        icon: "github",
       },
       {
-        url: 'https://gallery.fitbit.com/details/becef6eb-7fd3-4adb-a46c-9ada0287cf67',
-        icon: 'fitbit',
+        url: "https://raddle.netlify.app",
+        icon: "play",
       },
     ],
     imageContain: true,
-    bgColor: '#000',
-  },
-  {
-    imgSrc: { light: require('./images/mazegen.png'), dark: require('./images/mazegen.png') },
-    tags: ['Webapp', 'Typescript', 'Python', 'Full-stack'],
-    title: 'MazeGen',
-    tagline: 'Configurable maze generator',
-    description:
-      'User configurable maze generator for all your dungeoning needs (dragons not included)',
-    links: [
-      {
-        url: 'https://github.com/Chattox/MazeGen',
-        icon: 'github',
-        tooltip: 'Back-end',
-      },
-      {
-        url: 'https://github.com/Chattox/MazeGen-FE',
-        icon: 'github',
-        tooltip: 'Front-end',
-      },
-      {
-        url: 'https://chattox-mazegen.netlify.app',
-        icon: 'play',
-      },
-    ],
-    imageContain: true,
-    bgColor: '#FFF',
+    bgColor: "#FFF",
   },
   {
     imgSrc: {
-      light: require('./images/portfolio-light.png'),
-      dark: require('./images/portfolio-dark.png'),
+      light: require("./images/rainbow-fitbit.png"),
+      dark: require("./images/rainbow-fitbit.png"),
     },
-    tags: ['React', 'Typescript', 'Front-end'],
-    title: 'Portfolio',
+    tags: ["Fitbit", "JS", "Front-end"],
+    title: "Simple Rainbow",
+    tagline: "Custom Fitbit clockface",
+    description:
+      "Clean, simple, custom Fitbit clockface with time, date, battery % and real time heart rate display",
+    links: [
+      {
+        url: "https://github.com/chattox/fitbit-custom-clock-faces/tree/main/simple-rainbow",
+        icon: "github",
+      },
+      {
+        url: "https://gallery.fitbit.com/details/becef6eb-7fd3-4adb-a46c-9ada0287cf67",
+        icon: "fitbit",
+      },
+    ],
+    imageContain: true,
+    bgColor: "#000",
+  },
+  {
+    imgSrc: {
+      light: require("./images/mazegen.png"),
+      dark: require("./images/mazegen.png"),
+    },
+    tags: ["Webapp", "Typescript", "Python", "Full-stack"],
+    title: "MazeGen",
+    tagline: "Configurable maze generator",
+    description:
+      "User configurable maze generator for all your dungeoning needs (dragons not included)",
+    links: [
+      {
+        url: "https://github.com/Chattox/MazeGen",
+        icon: "github",
+        tooltip: "Back-end",
+      },
+      {
+        url: "https://github.com/Chattox/MazeGen-FE",
+        icon: "github",
+        tooltip: "Front-end",
+      },
+      {
+        url: "https://chattox-mazegen.netlify.app",
+        icon: "play",
+      },
+    ],
+    imageContain: true,
+    bgColor: "#FFF",
+  },
+  {
+    imgSrc: {
+      light: require("./images/portfolio-light.png"),
+      dark: require("./images/portfolio-dark.png"),
+    },
+    tags: ["React", "Typescript", "Front-end"],
+    title: "Portfolio",
     tagline: "What you're looking at right now!",
-    description: "You're already looking at it, but you can check out the code too",
+    description:
+      "You're already looking at it, but you can check out the code too",
     links: [
       {
-        url: 'https://github.com/Chattox/Chattox.github.io',
-        icon: 'github',
+        url: "https://github.com/Chattox/Chattox.github.io",
+        icon: "github",
       },
     ],
     imageContain: true,
